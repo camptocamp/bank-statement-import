f95cb7f7383cfe71b0b2fbd8879105012bb5cd4e
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-07-15 11:41:49 +0200

    Add account_statement_line_order

---
+++ account_statement_line_order/models/account_bank_statement_line.py
@@ -0,0 +1,32 @@
+# Copyright 2025 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from xmlrpc.client import MAXINT
+
+from odoo import api, models
+
+
+class AccountBankStatementLine(models.Model):
+    _inherit = "account.bank.statement.line"
+
+    @api.depends("journal_id.statement_line_internal_index")
+    def _compute_internal_index(self):  # pylint: disable=missing-return
+        """Compute the internal index based on the defined order."""
+        lines_computed = self.env["account.bank.statement.line"]
+        for line in self:
+            if not line._origin.id:
+                continue
+            elif (
+                line.journal_id.statement_line_internal_index
+                == "date_statement_id_line_sequence_id"
+            ):
+                line.internal_index = (
+                    f"{line.date.strftime('%Y%m%d')}"
+                    f"{line.statement_id.id:0>10}"
+                    f"{MAXINT - line.sequence:0>10}"
+                    f"{line._origin.id:0>10}"
+                )
+                lines_computed |= line
+        return super(
+            AccountBankStatementLine, self - lines_computed
+        )._compute_internal_index()
